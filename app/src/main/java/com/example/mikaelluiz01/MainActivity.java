ff4f57244bfb31c97050bca08f0c0045bf1e726c
Mikael Luiz <Mikael Luiz>
2023-09-17 23:24:56 -0300

    Atualização de arquivos

--- app/src/main/java/com/example/mikaelluiz01/MainActivity.java
+++ app/src/main/java/com/example/mikaelluiz01/MainActivity.java
@@ -7,9 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText insertText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnCalcular = (Button)this.findViewById(R.id.btnCalcular);
         Button btnLimpar = (Button)this.findViewById(R.id.btnCalcular);
-
         EditText insertText = (EditText)this.findViewById(R.id.insertText);
-        TextView txvLabel1 = (TextView)this.findViewById((R.id.txvLabel1));
+        EditText insertText2 = (EditText)this.findViewById(R.id.insertText2);
+
+        TextView txvLabel1 = (TextView)this.findViewById(R.id.txvLabel1);
+        TextView txvLabel2 = (TextView)this.findViewById(R.id.txvLabel2);
+        TextView textView6 = (TextView)this.findViewById(R.id.textView6);
+
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,9 +38,41 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String texto = insertText.getText().toString();
-                int tamanho = texto.length();
-                txvLabel1.setText(String.valueOf(tamanho).toString());
+                String texto = insertText.getText().toString().toLowerCase();
+                String caracter = insertText2.getText().toString().toLowerCase();
+                if (texto.equals("")){
+                    Toast.makeText(getApplicationContext(), "O campo de texto está vazio. Insira e Tente novamente!", Toast.LENGTH_LONG).show();
+                }else if(caracter.equals("")){
+                    Toast.makeText(getApplicationContext(), "O campo de caracter está vazio. Insira e Tente novamente!", Toast.LENGTH_LONG).show();
+                }else{
+                    //Calcula o numero de caracteres do texto
+                    int tamanho = texto.length();
+                    txvLabel1.setText(String.valueOf(tamanho).toString());
+
+                    //calcula o numero de caracters do caracter selecionado
+                    int contCaracter = 0;
+                    for(int i=0; i<tamanho; i++){
+                        if(texto.charAt(i)==caracter.charAt(0)){
+                            contCaracter++;
+                        }
+                    }
+                    txvLabel2.setText(String.valueOf(contCaracter).toString());
+                    textView6.setText("Numero de Caracteres '"+caracter+"'");
+                }
+                //Responsavel por remover o teclado quando o botão calcular for selecionado
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(insertText.getWindowToken(), 0);
+            }
+        });
+
+        //Responsavel por remover o teclado quando o elemento EditText perde o foco
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertText.clearFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(insertText.getWindowToken(), 0);
             }
         });
 
@@ -45,6 +84,5 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
     }
 }
